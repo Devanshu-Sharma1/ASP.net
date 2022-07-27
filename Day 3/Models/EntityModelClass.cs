64e60af69ce52e98dd05667694e5d123e6000aed
Devanshu-Sharma1 <41793756+Devanshu-Sharma1@users.noreply.github.com>
2022-07-27 15:59:34 +0530

    Add files via upload

---
+++ Day 3/Models/EntityModelClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EName { get; set; }
+        public string  Job { get; set; }
+        public string Sal { get; set; }
+        public string email { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
