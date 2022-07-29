371b12b0c30a2a12d722fa8f769382048c9c7d6a
Devanshu-Sharma1 <41793756+Devanshu-Sharma1@users.noreply.github.com>
2022-07-29 15:09:18 +0530

    Add files via upload

---
+++ Day 5-Task1/Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var Employees = await _context.Employees.ToListAsync();
+            if (Employees.Count > 0)
+            {
+                return Ok(Employees);
+
+            }
+            else
+            {
+                return Ok("No Employee exist in database");
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var Employees = await _context.Employees.FindAsync(id);
+
+            if (Employees != null)
+            {
+                return Ok(Employees);
+            }
+            else
+            {
+                return NotFound("Requested Employee Id does not exists.");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee obj)
+        {
+            await _context.Employees.AddAsync(obj);
+            await _context.SaveChangesAsync();
+            return Ok("New Employee details are saved in database.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(Employee obj)
+        {
+            _context.Employees.Update(obj);
+            await _context.SaveChangesAsync();
+            return Ok("Employee details are updated in database.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var obj = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(obj);
+            await _context.SaveChangesAsync();
+            return Ok("Employee details are deleted from database.");
+        }
+    }
+}
